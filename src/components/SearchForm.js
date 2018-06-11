5c306a0ab48477f2c1e942a7356292d78764f26c
Elgaoussi Hamza <rei-zer0@live.com>
2018-06-11 23:51:51 +0000

    Split cabin class and travellers in SearchForm

--- src/components/SearchForm.js
+++ src/components/SearchForm.js
@@ -7,8 +7,13 @@ import Checkbox from '@material-ui/core/Checkbox';
 import BottomNavigation from '@material-ui/core/BottomNavigation';
 import BottomNavigationAction from '@material-ui/core/BottomNavigationAction';
 import TextField from '@material-ui/core/TextField';
+import FormControl from '@material-ui/core/FormControl';
+import Select from '@material-ui/core/Select';
+import InputLabel from '@material-ui/core/InputLabel';
+import MenuItem from '@material-ui/core/MenuItem';
 import Button from '@material-ui/core/Button';
 import DateFnsUtils from 'material-ui-pickers/utils/date-fns-utils';
+import addDays from 'date-fns/addDays'
 import MuiPickersUtilsProvider from 'material-ui-pickers/utils/MuiPickersUtilsProvider';
 import DateTimePicker from 'material-ui-pickers/DateTimePicker';
 
@@ -43,16 +48,34 @@ class SearchForm extends Component {
     this.state = {
       navValue: 'flights',
       isOneWay: true,
+      cabinClass: 0,
+      cabinClassList: ['Economy', 'Business', 'First Class'],
+      numTravellers: 0,
       departDate: new Date(),
-      returnDate: new Date(),
+      returnDate: addDays(new Date(), 1)
     };
   }
 
   handleNavChange = (event, navValue) => {
     this.setState({ navValue });
+    if (navValue === 'flights') {
+      this.setState({cabinClassList: ['Economy', 'Business', '1st Class']});
+    } else if (navValue === 'trains') {
+      this.setState({cabinClassList: ['2nd Class', '1st Class', 'Single Bed']});
+    }
   }
   
-  handleOneWayChange = () => event => {
+  handleCabinClassChange = event => {
+    this.setState({ cabinClass: event.target.value });
+  }
+
+  handleNumTravellersChange = event => {
+    let num = event.target.value;
+    if (num >= 0 && num <= 10)
+      this.setState({ numTravellers: Math.floor(num) });
+  }
+
+  handleOneWayChange = event => {
     this.setState({ isOneWay: event.target.checked });
   }
 
@@ -66,7 +89,7 @@ class SearchForm extends Component {
 
   render() {
     const { classes } = this.props;
-    const { navValue, isOneWay, departDate, returnDate } = this.state;
+    const { navValue, numTravellers, cabinClass, cabinClassList, isOneWay, departDate, returnDate } = this.state;
     return (
       <MuiPickersUtilsProvider utils={DateFnsUtils}>
         <Grid
@@ -117,11 +140,14 @@ class SearchForm extends Component {
           </Grid>
           <Grid item xs={6}>
             <TextField
-              id='cabin'
-              label='Cabin Class & Travellers'
+              id="numTravellers"
+              label="Travellers"
+              value={numTravellers}
+              onChange={this.handleNumTravellersChange}
+              type="number"
               fullWidth
               InputLabelProps={{
-                shrink: true
+                shrink: true,
               }}
             />
           </Grid>
@@ -130,8 +156,8 @@ class SearchForm extends Component {
               control={
                 <Checkbox
                   checked={isOneWay}
-                  onChange={this.handleOneWayChange()}
-                  color='white'
+                  onChange={this.handleOneWayChange}
+                  color='default'
                 />
               }
               label='One way'
@@ -162,7 +188,26 @@ class SearchForm extends Component {
               onChange={this.handleReturnDateChange}
             />
           </Grid>
-          <Grid item xs={12} sm={8} className={classes.searchButton}>
+          {(navValue === 'flights' || navValue === 'trains') &&
+          <Grid item xs={6}>
+            <FormControl fullWidth >
+              <InputLabel htmlFor='cabinClass' shrink >Cabin Class</InputLabel>
+              <Select
+                value={cabinClass}
+                onChange={this.handleCabinClassChange}
+                inputProps={{
+                  name: 'cabinClass',
+                  id: 'cabinClass',
+                }}
+              >
+                {cabinClassList.map((cabin, i) => {
+                  return <MenuItem value={i} key={i.toString()}>{cabin}</MenuItem>
+                })}
+              </Select>
+            </FormControl>
+          </Grid>
+          }
+          <Grid item xs={6} className={classes.searchButton}>
             <Button type='submit' color='secondary' variant='contained' fullWidth>
               Search {navValue}
             </Button>
